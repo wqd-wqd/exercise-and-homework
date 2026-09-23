e376d539a695ef7487cb575f5e01673f273c7bd4
wqd <13713286758@163.com>
2026-09-23 09:48:17 +0800

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,20 @@
+/*有两个变量a和b，在执行了如下代码后：
+
+a = 1.000000001;
+
+b = a;
+
+printf("%.9f %.9f\n",a,b);
+
+输出两个数：1.000000001 1.000000000
+
+请问a和b分别是以下哪种类型？
+
+A. bool   B. char   C. short   D. int   E. float   F. double*/
+#include <stdio.h>
+ 
+int main()
+{
+    printf("F E\n");
+    return 0;
+}
