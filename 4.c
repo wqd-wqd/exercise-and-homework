a45946e0bf5743870bd26ba85a3ddb65781fd549
wqd <13713286758@163.com>
2026-09-23 09:43:53 +0800

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,20 @@
+/*有两个变量a和b，在执行了如下代码后：
+
+a =32768;
+
+b =a;
+
+printf("%d %d\n",a,b);
+
+输出两个数：32768 -32768
+
+请问a和b分别是以下哪种类型？
+
+A. bool   B. char   C. short   D. int   E. float   F. double*/
+#include <stdio.h>
+ 
+int main()
+{
+    printf("D C\n");
+    return 0;
+}
